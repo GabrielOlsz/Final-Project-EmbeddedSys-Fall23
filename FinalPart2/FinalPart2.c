cd36014201b6a7e3aee988b553bacf330df6582b
GabrielOlsz <olszgabe@gmail.com>
2023-11-06 20:19:21 -0500

    Working Merged FinalPart1 w/ SampleUSART

--- FinalPart2/FinalPart2.c
+++ FinalPart2/FinalPart2.c
@@ -13,14 +13,64 @@ unsigned int rcvrd;			// Container for received data
 
 void InitializeUSART1();	// Sub function which initializes the registers to enable USART1
 
+//PD2 = LEFT , PD4 = UP, PB5 = DOWN, PA6 = RIGHT, PC13 = CENTER
+
+unsigned long int pa6state = 0;
+unsigned long int pd2state = 0;
+unsigned long int pd4state = 0;
+unsigned long int pb5state = 0;
+unsigned long int pc13state = 0;
+
+void GPIODConfigOutput();
+void adcCONFIG();
+void Joystick();
+void GPIODConfigInput();
+unsigned int getAdcReading();
+
+
 //**************************************************************************************************
 //MAIN FUNCTION
 void main() {
+
+           RCC_APB2ENR |= 1 << 2;  // ENABLE GPIOA clock - necessary to use GPIOA
+           RCC_APB2ENR |= 1 << 3;  // ENABLE GPIOB clock - necessary to use GPIOB
+           RCC_APB2ENR |= 1 << 4;  // ENABLE GPIOC clock - necessary to use GPIOC
+           RCC_APB2ENR |= 1 << 5;  // ENABLE GPIOD clock - necessary to use GPIOD
+           RCC_APB2ENR |= 1 << 6;  // ENABLE GPIOE clock - necessary to use GPIOE
+
+           GPIOE_CRH = 0x33333333; //SETS GPIOE HIGH as output
+
+           GPIOA_CRL = 0x44444444; //SETS GPIOA LOW as input
+           GPIOA_CRH = 0x44444444; //SETS GPIOA HIGH as input
+
+           GPIOB_CRL = 0x44444444; //SETS GPIOB LOW as input
+           GPIOB_CRH = 0x44444444; //SETS GPIOB HIGH as input
+
+           GPIOC_CRL = 0x44444444; //SETS GPIOC LOW as input
+           GPIOC_CRH = 0x44444444; //SETS GPIOC HIGH as input
+
+           GPIOD_CRL = 0x44444444; //SETS GPIOD LOW as input
+           GPIOD_CRH = 0x44444444; //SETS GPIOD HIGH as input
+
+           adcCONFIG();
+
+
+
 	InitializeUSART1();		// Call sub function to initialize USART1
+
 
 //**************************************************************************************************
 //VARIABLE DECLARATIONS
 	for(;;) {
+
+                 GPIODConfigInput();
+                 Joystick();
+
+
+                GPIODConfigOutput();
+                GPIOD_ODR = getAdcReading();
+                delay_ms(100);
+
 		// The peripheral registers can be accessed by half-words (16-bit) or words (32-bit).
 		// Per data sheet (pg. 1007) USART1_SR consists of the following:
 		// 9   8   7   6   5   4    3   2  1  0
@@ -36,8 +86,9 @@ void main() {
 		//while transmitter data register is not empty wait
 		 while (! (USART1_SR & (1<<7)) == 0x80) {}
 		 // If we want to send data out via USART, we use the same data register
-		 USART1_DR = rcvrd-1;                // transmit the received data
+		 USART1_DR = rcvrd;                // transmit the received data
 	}
+
 }
 //**************************************************************************************************
 //SUB FUNCTIONS
@@ -66,3 +117,108 @@ void InitializeUSART1(){ // Sub function which initializes the registers to enab
 	Delay_ms(100);                  // Wait for USART to complete configuration and enable. This is
 									// not always necessary, but good practice.
 }
+
+
+
+//**************************************************************************************************
+void Joystick() {
+          if(GPIOA_IDR.B6 == 1 & pa6state == 0){        //Function for RIGHT button
+                          GPIOE_ODR = 0;
+                          pa6state = 1;
+          }
+          if(GPIOA_IDR.B6 == 0 & pa6state == 1){
+                          GPIOE_ODR.B9 = 1;
+                          GPIOE_ODR.B10 = 1;
+                          pa6state = 0;
+          }
+
+
+          if(GPIOB_IDR.B5 == 1 & pb5state == 0){         //Function for DOWN button
+                          GPIOE_ODR = 0;
+                          pb5state = 1;
+          }
+          if(GPIOB_IDR.B5 == 0 & pb5state == 1){
+                          GPIOE_ODR.B8 = 1;
+                          GPIOE_ODR.B12 = 1;
+                          pb5state = 0;
+          }
+
+
+
+
+
+          if(GPIOC_IDR.B13 == 1 & pc13state == 0){       // Function for Click button
+                           GPIOE_ODR = 0;
+                           pc13state = 1;
+          }
+          if(GPIOC_IDR.B13 == 0 & pc13state == 1){
+                           GPIOE_ODR = ~GPIOE_ODR;
+                           pc13state = 0;
+          }
+
+
+
+
+
+          if(GPIOD_IDR.B2 == 1 & pd2state == 0){         //Function for Left button
+                          GPIOE_ODR = 0;
+                          pd2state = 1;
+          }
+          if(GPIOD_IDR.B2 == 0 & pd2state == 1){
+                          GPIOE_ODR.B13 = 1;
+                          GPIOE_ODR.B14 = 1;
+                          pd2state = 0;
+          }
+
+
+
+
+
+
+          if(GPIOD_IDR.B4 == 1 & pd4state == 0){         //Function for UP button
+                          GPIOE_ODR = 0;
+                          pd4state = 1;
+          }
+          if(GPIOD_IDR.B4 == 0 & pd4state == 1){
+                          GPIOE_ODR.B11 = 1;
+                          GPIOE_ODR.B15 = 1;
+                          pd4state = 0;
+          }
+
+}
+
+//**************************************************************************************************
+void GPIODConfigInput(){
+
+     GPIOD_CRL = 0x44444444; //SETS GPIOD LOW as input
+     GPIOD_CRH = 0x44444444; //SETS GPIOD HIGH as input
+     GPIOD_ODR = 0;
+}
+
+void GPIODConfigOutput(){
+
+     GPIOD_CRL = 0x33333333; //SETS GPIOD LOW as output
+     GPIOD_CRH = 0x33333333; //SETS GPIOD HIGH as output
+     GPIOD_ODR = 0;
+}
+
+void adcCONFIG(){
+         RCC_APB2ENR |= 1 << 9 ; // Enable ADC1 Clock
+         GPIOC_CRL &= ~(0xF << 0); // Configure PC0 as an Analog Input
+         ADC1_SQR1 = (0b0000 << 20); // 1 conversion
+         ADC1_SQR3 = 10; // Select Channel 10 as only one in
+         ADC1_SMPR1 = 0b100; // Set sample time on channel 10
+         ADC1_CR2 |= (0b111 << 17); // Set software start as external event for
+         ADC1_CR2.ADON = 1; // Enable ADC1
+         delay_ms(10);
+}
+
+
+unsigned int getAdcReading(){
+
+    ADC1_CR2 |= (1 << 22) | (1 << 20);
+    while(!(ADC1_SR & 0b10));
+    return (((33*ADC1_DR)/1280)-5);   //Made an y=mx+b equation to convert from 256 to 4096 scale to a 1 to 100 scale for controlling game speed
+}
+
+ //****************************************************************************************
