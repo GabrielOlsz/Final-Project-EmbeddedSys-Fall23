30655008882e27efcc169e6b2464bee12bd41599
GabrielOlsz <olszgabe@gmail.com>
2023-11-29 18:32:15 -0500

    Progess on Objective 2

--- FinalPart3/FinalPart3.c
+++ FinalPart3/FinalPart3.c
@@ -8,33 +8,21 @@
 //**************************************************************************************************
 
 //**************************************************************************************************
-//INTERRUPT SERVICE ROUTINE
-void TIMER3_ISR () iv IVT_INT_TIM3 {
-  TIM3_SR.UIF = 0;  			// Reset UIF flag so next interrupt can be recognized when UIF is set
-  GPIOD_ODR = ~ GPIOD_ODR;	// Toggle PORTD LEDs
-  GPIOE_ODR = ~ GPIOE_ODR;	// Toggle PORTE LEDs
-}
-
-void TIMER1_ISR () {
-
-}
-
-
-//**************************************************************************************************
-//VARIABLE DECLARATIONS
+//VARIABLE/FUNCTION DECLARATIONS
 unsigned int rcvrd;                        // Container for received data
 unsigned int right[11] = {'R','T',' ','P','R','E','S','S','E','D', ' '};
 unsigned int left[11] = {'L','T',' ','P','R','E','S','S','E','D', ' '};
 unsigned int up[11] = {'U','P',' ','P','R','E','S','S','E','D', ' '};
 unsigned int down[11] = {'D','N',' ','P','R','E','S','S','E','D', ' '};
 unsigned int click[11] = {'C','K',' ','P','R','E','S','S','E','D', ' '};
-unsigned int pause[6] = {'P','A','U','S','E',' '};
 unsigned int unpause[9] = {'U','N','P','A','U','S','E','D',' '};
+unsigned int left7segdisplay[10] = {0XA000, 0XA100, 0XA400, 0XA500, 0XB000, 0XB100, 0XB400, 0XB500, 0XE000, 0XE100};
+unsigned int right7segdisplay[10] = {0XA000, 0XA100, 0XA400, 0XA500, 0XB000, 0XB100, 0XB400, 0XB500, 0XE000, 0XE100};
 unsigned int pauseToggle = 0;
 unsigned int i = 0;
-void InitializeUSART1();        // Sub function which initializes the registers to enable USART1
-void Timer1Configuration();
-void Timer3IntConfiguration();
+int counter = 0;
+int first;
+int second;
 
 //PD2 = LEFT , PD4 = UP, PB5 = DOWN, PA6 = RIGHT, PC13 = CENTER
 
@@ -49,11 +37,146 @@ void adcCONFIG();
 void Joystick();
 void GPIODConfigInput();
 unsigned int getAdcReading();
+void InitializeUSART1();        // Sub function which initializes the registers to enable USART1
+void Timer1Configuration();
+void Timer3IntConfiguration();
+void initializeGPIO();
+void PinConfiguration();
 
+//**************************************************************************************************
+//INTERRUPT SERVICE ROUTINE
+void TIMER3_ISR () iv IVT_INT_TIM3 {
+  TIM3_SR.UIF = 0;               // Reset UIF flag so next interrupt can be recognized when UIF is set
+ // GPIOD_ODR = ~GPIOD_ODR;        // Toggle PORTD LEDs
+  //GPIOE_ODR = ~GPIOE_ODR;        // Toggle PORTE LEDs
+}
+
+void TIMER1_ISR () iv IVT_INT_TIM1_UP {
+     TIM1_SR.UIF = 0;               //Reset UIF flag
+     GPIOE_ODR = ~GPIOE_ODR;
+     counter++;
+     
+     //split counter into two numbers, ex: 25 will split to 2 and 5
+     first = counter / 10;
+     second = counter % 10;
+     
+     if (first == 0) {
+        GPIOE_ODR = left7segdisplay[0];
+     }
+     if (first == 1) {
+        GPIOE_ODR = left7segdisplay[1];
+     }
+     if (first == 2) {
+        GPIOE_ODR = left7segdisplay[2];
+     }
+     if (first == 3) {
+        GPIOE_ODR = left7segdisplay[3];
+     }
+     if (first == 4) {
+        GPIOE_ODR = left7segdisplay[4];
+     }
+     if (first == 5) {
+        GPIOE_ODR = left7segdisplay[5];
+     }
+     if (first == 6) {
+        GPIOE_ODR = left7segdisplay[6];
+     }
+     if (first == 7) {
+        GPIOE_ODR = left7segdisplay[7];
+     }
+     if (first == 8) {
+        GPIOE_ODR = left7segdisplay[8];
+     }
+     if (first == 9) {
+        GPIOE_ODR = left7segdisplay[9];
+     }
+
+     if (second == 0) {
+        GPIOE_ODR = right7segdisplay[0];
+     }
+     if (second == 1) {
+        GPIOE_ODR = right7segdisplay[1];
+     }
+     if (second == 2) {
+        GPIOE_ODR = right7segdisplay[2];
+     }
+     if (second == 3) {
+        GPIOE_ODR = right7segdisplay[3];
+     }
+     if (second == 4) {
+        GPIOE_ODR = right7segdisplay[4];
+     }
+     if (second == 5) {
+        GPIOE_ODR = right7segdisplay[5];
+     }
+     if (second == 6) {
+        GPIOE_ODR = right7segdisplay[6];
+     }
+     if (second == 7) {
+        GPIOE_ODR = right7segdisplay[7];
+     }
+     if (second == 8) {
+        GPIOE_ODR = right7segdisplay[8];
+     }
+     if (second == 9) {
+        GPIOE_ODR = right7segdisplay[9];
+     }
+
+
+}
 
 //**************************************************************************************************
 //MAIN FUNCTION
 void main() {
+           initializeGPIO();            //Enable port clocks
+           adcCONFIG();                 //Configure ADC read
+           InitializeUSART1();          //Configure USART1
+
+        for(;;) {
+
+                 GPIODConfigInput();            //Function to call GPIOD config input
+                 Joystick();                    //Function for joystick
+                 Timer1Configuration();
+                 Timer3IntConfiguration();
+                 GPIODConfigOutput();           //Function to call GPIOD config output
+                 GPIOD_ODR = getAdcReading();   //Display ADC signal to GPIOD LEDS
+                 delay_ms(100);
+
+
+                 if(((USART1_SR & (1<<5))== 0x20)){
+                  if(USART1_DR == 'q' || USART1_DR == 'Q'){
+                     USART1_DR = counter;
+                  }
+                 }
+
+                if(((USART1_SR & (1<<5))== 0x20)){
+
+                  if (pauseToggle == 0 && (USART1_DR == 'p' || USART1_DR == 'P')){
+                    i = 0;
+                    pauseToggle = 1;
+                  }
+
+                  else if(pauseToggle == 1 && (USART1_DR == 'p' || USART1_DR == 'P')){
+                    i = 2;
+                    pauseToggle = 0;
+
+                 }
+                  for (i = i; i<9; i++){
+                      while(USART1_SR.TC == 0) {}
+                      USART1_DR = unpause[i];              //Prints out unpause
+                  }
+
+                }
+
+        }
+
+}
+
+
+//**************************************************************************************************
+//SUB FUNCTIONS
+
+void initializeGPIO(){
 
            RCC_APB2ENR |= 1 << 2;  // ENABLE GPIOA clock - necessary to use GPIOA
            RCC_APB2ENR |= 1 << 3;  // ENABLE GPIOB clock - necessary to use GPIOB
@@ -73,6 +196,7 @@ void main() {
            GPIOC_CRH = 0x44444444; //SETS GPIOC HIGH as input
 
 
+<<<<<<< Updated upstream
            adcCONFIG();
 
 
@@ -113,9 +237,11 @@ void main() {
 
         }
 
+=======
+>>>>>>> Stashed changes
 }
-//**************************************************************************************************
-//SUB FUNCTIONS
+
+
 void InitializeUSART1(){ // Sub function which initializes the registers to enable USART1
         RCC_APB2ENR |= 1;                 // Enable clock for Alt. Function. USART1 uses AF for PA9/PA10
         AFIO_MAPR=0X0F000000;             // Do not mask PA9 and PA10 (becaue we are using for USART)
@@ -267,38 +393,41 @@ unsigned int getAdcReading(){
 
 
 void Timer1Configuration(){
-	RCC_APB2ENR |= (1 << 11);   // Enable TIMER1 clock. RCC: Clock Configuration Register
-								// Different clocks may use different registers.
-								// Ex. TIMER4 uses RCC_APB1ENR
-	TIM1_CR1 = 0x0000;  // Disable timer until configuration is complete
-						// If reset value of RCC_CFGR is used, then the 8MHz clock will
-						// be the clock source for timer
-	TIM1_PSC = 7999;    // Clock to TIMx_CNT = 72000000 (clock applied to prescaler register) /
-					    //                     7999 (Value in TIMx_PSC) + 1) = 9000
-	TIM1_ARR = 9000;	// Reload timer count register with this value when count register resets
-						// to 0 after counting from zero to this value
-	TIM1_CR1 = 0x0001; 	// Enable TIMER1
+        RCC_APB2ENR |= (1 << 11);   // Enable TIMER1 clock. RCC: Clock Configuration Register
+                                                                // Different clocks may use different registers.
+                                                                // Ex. TIMER4 uses RCC_APB1ENR
+        TIM1_CR1 = 0x0000;  // Disable timer until configuration is complete
+                                                // If reset value of RCC_CFGR is used, then the 8MHz clock will
+                                                // be the clock source for timer
+        TIM1_PSC = 14399;    // Clock to TIMx_CNT = 72000000/(14399+1) = 5000
+        TIM1_ARR = 5000;        // Reload timer count register with this value when count register resets
+                                                // to 0 after counting from zero to this value
+        NVIC_ISER0.B25 = 1;     // Enable global interrupt for TIMER1 in NVIC
+        TIM1_DIER.UIE = 1;     // Update interrupt enable
+        TIM1_CR1 = 0x0001;         // Enable TIMER1
 
 // Notice: Bit 4 of TIM1_CR1 specifies whether the counter count up (BIT4=0) or counts down (BIT4=1)
 // In this configuration this counting up is used.
 }
 
 void Timer3IntConfiguration(){
-	RCC_APB1ENR |= (1 << 1);// Enable TIMER3 clock. RCC: Clock Configuration Register
-	TIM1_CR1 = 0x0000;  	// Disable timer until configuration is complete
-							// If reset value of RCC_CFGR is used, then the 8MHz clock will
-							// be the clock source for timer
-	TIM1_PSC = 7999;    	// Clock to TIMx_CNT = 72000000 (clock applied to prescaler register) /
-							//                     7999 (Value in TIMx_PSC) + 1) = 9000
-	TIM1_ARR = 9000;		// Reload timer count register with this value when count register resets
-							// to 0 after counting from zero to this value
-	NVIC_ISER0 |= 1<<29;	// Enable global interrupt for TIMER3 in NVIC
-							// Interrupt set enable register 0. Position of this interrupt in vector
-							// table is 29, so set the corresponding bit in interrupt service enable
-							// register 0. This is a 32 bit register. ISER1 is used for interrupt
-							// numbers greater than 31.
-	TIM3_DIER.UIE = 1;  	// Update interrupt enable
-	TIM3_CR1 = 0x0001; 		// Enable TIMER3
+        RCC_APB1ENR |= (1 << 1);// Enable TIMER3 clock. RCC: Clock Configuration Register
+        TIM3_CR1 = 0x0000;          // Disable timer until configuration is complete
+                                                        // If reset value of RCC_CFGR is used, then the 8MHz clock will
+                                                        // be the clock source for timer
+        TIM3_PSC = 7999;            // Clock to TIMx_CNT = 72000000 (clock applied to prescaler register) /
+                                                        //                     7999 (Value in TIMx_PSC) + 1) = 9000
+        TIM3_ARR = 9000;                // Reload timer count register with this value when count register resets
+                                                        // to 0 after counting from zero to this value
+        NVIC_ISER0.B29 = 1;        // Enable global interrupt for TIMER3 in NVIC
+                                                        // Interrupt set enable register 0. Position of this interrupt in vector
+                                                        // table is 29, so set the corresponding bit in interrupt service enable
+                                                        // register 0. This is a 32 bit register. ISER1 is used for interrupt
+                                                        // numbers greater than 31.
+        TIM3_DIER.UIE = 1;          // Update interrupt enable
+        TIM3_CR1 = 0x0001;          // Enable TIMER3
 }
+
+
 
  //****************************************************************************************
